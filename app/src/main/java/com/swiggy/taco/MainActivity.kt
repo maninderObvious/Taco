0af8f084689d84f9c7e237f2c1b0786f9175e9e6
maninderDefault <82432241+maninderDefault@users.noreply.github.com>
2021-04-28 21:21:01 +0530

    card

--- app/src/main/java/com/swiggy/taco/MainActivity.kt
+++ app/src/main/java/com/swiggy/taco/MainActivity.kt
@@ -2,12 +2,10 @@ package com.swiggy.taco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.swiggy.taco.card.card
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        card(this, attrs = null, R.attr.materialCardViewStyle)
     }
 }
