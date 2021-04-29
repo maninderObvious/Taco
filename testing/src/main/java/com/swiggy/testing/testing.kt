e33b1b54c992f2ffd80de3090296a36893c9b965
maninderDefault <82432241+maninderDefault@users.noreply.github.com>
2021-04-29 11:47:19 +0530

    init

---
+++ testing/src/main/java/com/swiggy/testing/testing.kt
@@ -0,0 +1,13 @@
+package com.swiggy.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swiggy.taco.card.Card
+
+class testing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+
+    }
+}
